c466cea98dcea5ee239d9421f0ff0f0bfcdedc10
Zachary Arterburn <yarchaz.art@gmail.com>
2019-10-30 21:00:18 -0600

    commit

--- lesson7/lazy.js
+++ lesson7/lazy.js
@@ -29,32 +29,3 @@ if('IntersectionObserver' in window) {
     });
 }
 
-/*const images = document.querySelectorAll('[data-src]');
-
-function preloadImage(img){
-    const src = img.getAttribute('data-src');
-    if(!src){
-        return;
-    }
-    img.src = src;
-}
-
-const imgOptions = {
-    threshold:1,
-    rootMargin: "0px 0px 50px 0px"
-};
-
-const imgObserver = new IntersectionObserver((enteries, imgObserver) => {
-    enteries.forEach(entry =>{
-        if(!entry.isIntersecting){
-            return;
-        }else{
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target)
-        }
-    })
-}, imgOptions);
-
-images.forEach(img => {
-    imgObserver.observe(image);
-});*/
