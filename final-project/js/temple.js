d777932698e22b72f4e16e5a7ab71762139ac8d4
Zachary Arterburn <yarchaz.art@gmail.com>
2019-12-18 13:01:52 -0700

    commit

--- final-project/js/temple.js
+++ final-project/js/temple.js
@@ -5,32 +5,50 @@ fetch(requestURL)
     })
     .then(function(jsonObject){
         console.table(jsonObject);
-        const temples = jsonObject['towns'];
-        for(let i=0; i<towns.length; i++){
-          if(towns[i].name ==="Fish Haven" || towns[i].name ==="Preston" || towns[i].name === "Soda Springs"){  
+        const temples = jsonObject['temples'];
+        for(let i=0; i<temples.length; i++){
           let card = document.createElement('section');
-          let h2 = document.createElement('h2');
-          let motto = document.createElement('h4');
-          let year = document.createElement('p');
-          let pop = document.createElement('p');
-          let averageR = document.createElement('p');
-          let img = document.createElement('img'); 
-          let text = document.createElement('div');
-          h2.textContent= towns[i].name;
-          motto.textContent = towns[i].motto;
-          year.textContent = "Year Founded: " + towns[i].yearFounded;
-          pop.textContent ="Current Population: " + towns[i].currentPopulation;
-          averageR.textContent ="Average Rainfall: " + towns[i].averageRainfall;
-          img.setAttribute('src','images/'+towns[i].photo);
-          img.setAttribute('alt',towns[i].name + " image");
-          text.appendChild(h2);
-          text.appendChild(motto);
-          text.appendChild(year);
-          text.appendChild(pop);
-          text.appendChild(averageR);
-          card.appendChild(text);
-          card.appendChild(img);
-          document.querySelector('div.cards').appendChild(card);
-          }else{continue}
+          let temple = document.createElement('h3');
+          let photo = document.createElement('img');
+          let dedicated = document.createElement('h4');
+          let address = document.createElement('p');
+          let phone = document.createElement('p');
+          let email = document.createElement('p');
+          let services = document.createElement('div');
+          let schedule = document.createElement('p');
+          let closures = document.createElement('div');
+          let tmain = document.createElement('div');
+          let h3img = document.createElement('div');
+          temple.textContent = temples[i].name;
+          photo.setAttribute('src','images/'+temples[i].photo);
+          photo.setAttribute('alt',temples[i].name + " image");
+          dedicated.textContent = "Dedicated" + " " + temples[i].dedicated;
+          address.textContent = temples[i].address;
+          phone.textContent= temples[i].phone;
+          email.textContent = temples[i].email;
+          for (x=0;x<temples[i].services.length; x++){
+              let service = document.createElement('p');
+              service.textContent=temples[i].services[x];
+              services.appendChild(service); 
+          }
+          schedule.textContent = temples[i].eSchedule;
+
+          for (z=0;z<temples[i].tClosures.length; z++){
+              let close = document.createElement("p");
+              close.textContent = temples[i].tClosures[z];
+              closures.appendChild(close);
+          }
+          h3img.appendChild(temple);
+          h3img.appendChild(photo);
+          tmain.appendChild(dedicated);
+          tmain.appendChild(address);
+          tmain.appendChild(phone);
+          tmain.appendChild(email);
+          tmain.appendChild(services)
+          tmain.appendChild(schedule);
+          tmain.appendChild(closures);
+          card.appendChild(h3img);
+          card.appendChild(tmain);
+          document.querySelector('div.templeCard').appendChild(card);
         }
     });
